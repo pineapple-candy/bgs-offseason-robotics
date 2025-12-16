8bd922d7948998da9b410a482821d740888be3da
pineapple-candy <guanao675209@brisbanegrammar.com>
2025-12-16 23:13:11 +1100

    Finished index.html rough idea - started hyperlinking to history

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ function type() {
 }
 
 type();
+
+// Countdown (thanks website!)
+
+var countDownDate = new Date("April 5, 2026 15:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "SEASON RELEASED!";
+  }
+}, 1000);
