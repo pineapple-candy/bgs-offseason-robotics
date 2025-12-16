d13ad4ef3fabfd4cb2e7c98082bde07357127d9c
pineapple-candy <guanao675209@brisbanegrammar.com>
2025-12-16 17:24:09 +1100

    Made a start on the beg. page

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const button = document.getElementById("btn");
+// Script for typewriter effect.
+const text = "What is OFFSEASON?";
+const speed = 100;      // typing speed (ms)
+const deleteSpeed = 60; // deleting speed
+const pause = 1000;     // pause before delete / retype
 
-button.addEventListener("click", () => {
-    alert("Button clicked!");
-});
+let i = 0;
+let isDeleting = false;
+const el = document.getElementById("typewriter");
+
+function type() {
+  if (!isDeleting) {
+    el.textContent = text.slice(0, i++);
+    if (i > text.length) {
+      setTimeout(() => isDeleting = true, pause);
+    }
+  } else {
+    el.textContent = text.slice(0, i--);
+    if (i === 0) {
+      isDeleting = false;
+    }
+  }
+
+  setTimeout(type, isDeleting ? deleteSpeed : speed);
+}
+
+type();
